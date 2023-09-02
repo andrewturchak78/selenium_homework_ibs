ea9743d63e6c4a1815968cd89a9ab9a72b98c14b
Andrey Turchak <andrewturchak78@mail.ru>
2023-09-02 23:02:36 +0300

    test fixed

--- src/test/java/testDB/DBTest.java
+++ src/test/java/testDB/DBTest.java
@@ -1,7 +1,9 @@
 package testDB;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,35 +12,44 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 public class DBTest {
     private static String url = "jdbc:h2:tcp://localhost:9092/mem:testdb";
     private static String user = "user";
-    private static String pass = "pass";
+    private static String password = "pass";
 
-    private static Connection getConn() throws SQLException {
-        return DriverManager.getConnection(url,user,pass);
+    private static DataSource getH2DataSource() throws SQLException {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setURL(url);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+        return dataSource;
     }
 
     @Test
     void test() throws SQLException {
-        Statement statement = getConn().createStatement();
-        int addPotato = statement.executeUpdate("INSERT INTO food VALUES (5, 'Картофель', 'Vegetable', false)");
-
-        PreparedStatement searchPotato = getConn()
-                .prepareStatement("select * from food where food_name = 'Картофель'");
-        try (ResultSet rs1 = searchPotato.executeQuery()) {
-            while (rs1.next()) {
-                assertEquals("5", rs1.getString("FOOD_ID"));
-                assertEquals("Картофель", rs1.getString("FOOD_NAME"));
-                assertEquals("Vegetable", rs1.getString("FOOD_TYPE"));
-                assertEquals("0", rs1.getString("FOOD_EXOTIC"));
+        Connection conn = getH2DataSource().getConnection();
+        try (Statement statement = conn.createStatement()) {
+            int addPotato = statement.executeUpdate("INSERT INTO food VALUES (5, 'Картофель', 'Vegetable', false)");
+        }
+        try (PreparedStatement searchPotato = conn
+                .prepareStatement("select * from food where food_name = 'Картофель'")) {
+            try (ResultSet rs1 = searchPotato.executeQuery()) {
+                while (rs1.next()) {
+                    assertEquals("5", rs1.getString("FOOD_ID"));
+                    assertEquals("Картофель", rs1.getString("FOOD_NAME"));
+                    assertEquals("Vegetable", rs1.getString("FOOD_TYPE"));
+                    assertEquals("0", rs1.getString("FOOD_EXOTIC"));
+                }
             }
         }
-        int delPotato = statement.executeUpdate("DELETE FROM food WHERE food_id = 5");
-        PreparedStatement searchPotatoNotExists = getConn()
-                .prepareStatement("select * from food where food_id = 5");
-        try (ResultSet rs2 = searchPotatoNotExists.executeQuery()) {
-            while (rs2.next()) {
-                assertNull(rs2.getString("FOOD_ID"));
+        try (Statement statement = conn.createStatement()) {
+            int delPotato = statement.executeUpdate("DELETE FROM food WHERE food_id = 5");
+        }
+        try (PreparedStatement searchPotatoNotExists = conn
+                .prepareStatement("select * from food where food_id = 5")) {
+            try (ResultSet rs2 = searchPotatoNotExists.executeQuery()) {
+                while (rs2.next()) {
+                    assertNull(rs2.getString("FOOD_ID"));
+                }
             }
         }
-        getConn().close();
+        conn.close();
     }
 }
